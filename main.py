e4ad00474af7f67ba15a87bd7e6a940ac3257e1e
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-09-28 21:51:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#removeing and adding items from a list
+
+#list
+x = [
+  231, 435, 777, 9874, 666
+  ]
+
+#removeing
+
+y = int(input("what number do you want to remove?: "))
+
+new_x =[x for x in x if x != y ]
+
+print(new_x)
+
+#adding
+
+j = int(input("what number would you like to add?: "))
+
+#remember that you have to specify a posision that the new item will take in the list - in this case its at posision 0 - or the start of the list
+
+new_x.insert(0,j)    
+
+print(new_x)
+
